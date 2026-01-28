781cf53f108f4935185159e9d01e1db37243d02c
Whiz225 <nwosuemmanuel1994@gmail.com>
2026-01-28 07:59:45 +0100

    Fix deployment issues

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+  // env: {
+  //   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  // },
   images: {
     remotePatterns: [
       {
